fe767c6004fffd2ac04a9f1c390b9a9006795d4a
Gera Garza <josegerag@hotmail.com>
2020-05-20 03:55:35 -0500

    new files

--- js/script.js
+++ js/script.js
@@ -1,71 +1,43 @@
-var currentEvent = "rotation-start";
+var sky_ele = document.getElementById("sky");
+var html_ele = document.getElementById("html");
+var body_ele = document.getElementById("body");
+var inside = localStorage.sky;
 
+resizeIframe();
+function resizeIframe() {
+  wid = window.innerWidth;
+  var y = 100 + (wid - 2000) * 0.03
+  new_width = (120 / y) * 50
 
-t = setTimeout(approach, 2000);
+  if (window.innerWidth < 700) { new_width = 120; }
 
-t = setInterval(walkacross, 10000);
+  var ele = document.getElementById("camera");
 
-var counter = 1;
-var toggle = true;
-
-function approach(){
-    document.getElementById("thing").setAttribute("animation-mixer", "clip:idle");
-  
-}
-
-async function walkacross() {
-
-  if (toggle) {
-
-    // property: position; from:-2 -2 -6; to: 2 -2 -6 ; dur: 2000; easing: linear; dir: alternate
-    document.getElementById("thing").setAttribute("rotation", "0 270 0");
-    document.getElementById("thing").setAttribute("animation-mixer", "clip:idle");
-    document.getElementById("thing").setAttribute("animation", "property: position; from:3 -2 -6; to: 3 -2 -6 ; dur: 4000; easing: linear;  ");
-
-    await setTimeout(function () {
-      document.getElementById("thing").setAttribute("animation-mixer", "clip:walk");
-      document.getElementById("thing").setAttribute("animation", "property: position; from:3 -2 -6; to: -3 -2 -6 ; dur: 4000; easing: linear;  ");
-    }, 6000);
-
-    toggle = false;
-  } else {
-
-    document.getElementById("thing").setAttribute("rotation", "0 90 0");
-    document.getElementById("thing").setAttribute("animation-mixer", "clip:idle");
-    document.getElementById("thing").setAttribute("animation", "property: position; from:-3 -2 -6; to: -3 -2 -6 ; dur: 4000; easing: linear;  ");
-
-    await setTimeout(function () {
-      document.getElementById("thing").setAttribute("animation-mixer", "clip:walk");
-      document.getElementById("thing").setAttribute("animation", "property: position; from:-3 -2 -6; to: 3 -2 -6 ; dur: 4000; easing: linear;  ");
-    }, 6000);
-
-    toggle = true;
+  if (ele) {
+    ele.setAttribute("camera", "fov:" + new_width);
   }
-
-
-};
-
-var param1 = getParamValue('param1');
-
-function getParamValue(paramName)
-{
-    var url = window.location.search.substring(1); //get rid of "?" in querystring
-    var qArray = url.split('&'); //get key-value pairs
-    for (var i = 0; i < qArray.length; i++) 
-    {
-        var pArr = qArray[i].split('='); //split key and value
-        if (pArr[0] == paramName) 
-          var fov = parseInt(pArr[1], 10);
-          if(isNaN(fov)){
-            fov = 70;
-          }
-          console.log(fov)
-          document.getElementById("camera").setAttribute("camera", "fov:"+ fov);
-    }
-
-
 }
 
 
+var checkExist = setInterval(function () {
+
+  if (html_ele == null) {
+    clearInterval(checkExist);
+  }
+  else if (html_ele && body_ele) {
+    clearInterval(checkExist);
+    delete_cover();
+
+  }
+}, 100);
 
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function delete_cover() {
+  // await sleep(3000);
+  html_ele.setAttribute("style", "overflow:visible; height:auto;");
+  body_ele.setAttribute("style", "overflow:visible; height:auto");
+  $('#cover').remove();
+}
